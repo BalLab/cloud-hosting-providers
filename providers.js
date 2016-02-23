6f1f1b499120c81d1feaadfe7ef98e21f2ad9ce5
Radomir Kujović <kujovic@users.noreply.github.com>
2016-02-23 15:45:51 +0100

    Create providers.js

---
+++ providers.js
@@ -0,0 +1,317 @@
+var providers = {
+	"A2Hosting": {
+		url: "https://www.a2hosting.com/",
+		software: "CloudStack",
+		locations: "US"
+	},
+	"Acquia": {
+		url: "https://www.acquia.com/",
+		software: "CloudStack",
+		locations: "US, SG, DE, IR, BR, AU, JP"
+	},
+	"AlecSoft": {
+		url: "http://www.alecsoft.net/",
+		software: "OpenStack",
+		locations: "US, UK"
+	},
+	"Anchor": {
+		url: "http://www.anchor.com.au/",
+		software: "OpenStack",
+		locations: "AU"
+	},
+	"Auro": {
+		url: "https://auro.io/",
+		software: "OpenStack",
+		locations: "CA"
+	},
+	"Axiomio": {
+		url: "http://axiomio.com/",
+		software: "CloudStack",
+		locations: "US, CA"
+	},
+	"BareMetalCloud": {
+		url: "https://www.baremetalcloud.com/",
+		software: "OpenStack",
+		locations: "US"
+	},
+	"BiznisHosting": {
+		url: "http://biznishosting.net/",
+		software: "CloudStack",
+		locations: "RS"
+	},
+	"BlueBox (IBM)": {
+		url: "https://www.blueboxcloud.com/",
+		software: "OpenStack",
+		locations: "US, CA"
+	},
+	"Canadian Web Hosting": {
+		url: "https://www.canadianwebhosting.com/",
+		software: "OpenStack",
+		locations: "CA"
+	},
+	"CentriLogic": {
+		url: "http://www.centrilogic.com/",
+		software: "CloudStack",
+		locations: "US, CA, UK"
+	},
+	"City Network Hosting": {
+		url: "https://www.citynetworkhosting.com/",
+		software: "OpenStack",
+		locations: "UK, SE, US"
+	},
+	"Cloud Sigma": {
+		url: "https://www.cloudsigma.com/",
+		software: "CloudStack",
+		locations: "CH"
+	},
+	"CloudOps": {
+		url: "http://www.cloudops.com/",
+		software: "CloudStack",
+		locations: "CA"
+	},
+	"CloudVPS": {
+		url: "http://www.cloudvps.com/",
+		software: "OpenStack",
+		locations: "NL"
+	},
+	"Cloudwatt": {
+		url: "https://www.cloudwatt.com/en/",
+		software: "OpenStack",
+		locations: "FR"
+	},
+	"Codero": {
+		url: "http://www.codero.com/",
+		software: "CloudStack",
+		locations: "US"
+	},
+	"Data Centred": {
+		url: "http://www.datacentred.co.uk/",
+		software: "OpenStack",
+		locations: "UK"
+	},
+	"Data Ways": {
+		url: "http://www.dataways.gr/en/",
+		software: "CloudStack",
+		locations: "CH, GR"
+	},
+	"DataRealm": {
+		url: "https://www.datarealm.com/",
+		software: "CloudStack",
+		locations: "US"
+	},
+	"Dedify": {
+		url: "https://www.dedify.com/",
+		software: "CloudStack",
+		locations: "DE"
+	},
+	"Digital Ocean": {
+		url: "https://www.digitalocean.com/",
+		software: "CloudStack",
+		locations: "NL"
+	},
+	"Dreamhost": {
+		url: "https://www.dreamhost.com/",
+		software: "OpenStack",
+		locations: "US"
+	},
+	"Elastx": {
+		url: "http://elastx.com/",
+		software: "OpenStack",
+		locations: "SE"
+	},
+	"Enter Cloud Suite": {
+		url: "http://www.entercloudsuite.com/en/",
+		software: "OpenStack",
+		locations: "IT, NL, DE"
+	},
+	"Exoscale": {
+		url: "https://www.exoscale.ch/",
+		software: "CloudStack",
+		locations: "CH"
+	},
+	"e-shelter": {
+		url: "https://e-shelter.de/",
+		software: "CloudStack",
+		locations: "DE, CH, AT"
+	},
+	"Flint Hosts": {
+		url: "http://www.flinthosts.co.uk/",
+		software: "CloudStack",
+		locations: "UK"
+	},
+	"FortaCloud": {
+		url: "http://fortacloud.co/",
+		software: "CloudStack",
+		locations: "NL, US, PA, BR, HK, JP"
+	},
+	"Green House Data": {
+		url: "https://www.greenhousedata.com/",
+		software: "CloudStack",
+		locations: "US"
+	},
+	"iKoula": {
+		url: "https://express.ikoula.com/en/public-cloud",
+		software: "CloudStack",
+		locations: "FR, DE, CH, NL, LU"
+	},
+	"Internap": {
+		url: "http://www.internap.com/",
+		software: "OpenStack",
+		locations: "UK, NL, US, CA, HK, JP, SG"
+	},
+	"Interned Services": {
+		url: "https://www.is.nl/en",
+		software: "CloudStack",
+		locations: "NL"
+	},
+	"Interoute": {
+		url: "https://cloudstore.interoute.com/main/",
+		software: "CloudStack",
+		locations: "UK, DE, FR, CH, IT, ES"
+	},
+	"IO": {
+		url: "https://www.io.com/",
+		software: "OpenStack",
+		locations: "US, UK, SG"
+	},
+	"Iomart": {
+		url: "http://www.iomart.com/",
+		software: "OpenStack",
+		locations: "US, UK, SG, AE"
+	},
+	"iwStack": {
+		url: "http://iwstack.com/",
+		software: "CloudStack",
+		locations: "IT"
+	},
+	"KoolFit": {
+		url: "https://koolfit.mx/",
+		software: "OpenStack",
+		locations: "MX"
+	},
+	"Leaseweb": {
+		url: "https://www.leaseweb.com/",
+		software: "CloudStack",
+		locations: "NL"
+	},
+	"M5Cloud": {
+		url: "http://www.m5cloud.com/",
+		software: "CloudStack",
+		locations: "US"
+	},
+	"Memset": {
+		url: "http://www.memset.com/",
+		software: "OpenStack",
+		locations: "UK, US"
+	},
+	"Miracloud": {
+		url: "http://www.contegix.com/",
+		software: "CloudStack",
+		locations: "US, NL"
+	},
+	"Mirantis": {
+		url: "https://www.mirantis.com/",
+		software: "OpenStack",
+		locations: "US, CN"
+	},
+	"Morphlabs": {
+		url: "http://morphlabs.com/",
+		software: "OpenStack",
+		locations: "PH"
+	},
+	"Numergy": {
+		url: "https://www.numergy.com/",
+		software: "OpenStack",
+		locations: "FR"
+	},
+	"NTSource": {
+		url: "http://www.ntsource.com/",
+		software: "CloudStack",
+		locations: "US"
+	},
+	"OpenMinds": {
+		url: "https://www.openminds.be/en",
+		software: "CloudStack",
+		locations: "BE"
+	},
+	"OVH": {
+		url: "https://www.ovh.com/",
+		software: "OpenStack",
+		locations: "FR, CA"
+	},
+	"Polcom": {
+		url: "http://www.polcom.com.pl/en",
+		software: "CloudStack",
+		locations: "PL"
+	},
+	"Rackspace": {
+		url: "http://www.rackspace.com/",
+		software: "OpenStack",
+		locations: "UK, US, HK, AU"
+	},
+	"RedBridge": {
+		url: "https://cloud.redbridge.se/cloud-services",
+		software: "CloudStack",
+		locations: "SE"
+	},
+	"Safe Swiss Cloud": {
+		url: "https://www.safeswisscloud.ch/en",
+		software: "CloudStack",
+		locations: "CH"
+	},
+	"Seflow": {
+		url: "http://www.seflow.net/",
+		software: "OpenStack",
+		locations: "IT"
+	},
+	"Stacksquare": {
+		url: "http://www.stacksquare.com/",
+		software: "CloudStack",
+		locations: "IT"
+	},
+	"StratoGen": {
+		url: "http://www.stratogen.net/",
+		software: "OpenStack",
+		locations: "US, UK, AU, SG"
+	},
+	"Telia Latvija": {
+		url: "http://www.telia.lv/en/",
+		software: "CloudStack",
+		locations: "LV"
+	},
+	"TeutoStack": {
+		url: "https://teutostack.de/",
+		software: "OpenStack",
+		locations: "DE"
+	},
+	"Tranquil Hosting": {
+		url: "http://www.tqhosting.com/",
+		software: "CloudStack",
+		locations: "US, DE"
+	},
+	"Ultimum": {
+		url: "http://ultimum.io/",
+		software: "OpenStack",
+		locations: "CZ"
+	},
+	"Umbee Hosting": {
+		url: "http://www.umbeehosting.net/",
+		software: "CloudStack",
+		locations: "US, UK, AU"
+	},
+	"UStack": {
+		url: "https://www.ustack.com/",
+		software: "OpenStack",
+		locations: "CN, US"
+	},
+	"VDI Space": {
+		url: "https://vdi.space/",
+		software: "CloudStack",
+		locations: "US"
+	},
+	"Verio": {
+		url: "http://www.verio.com/cloud-computing/",
+		software: "CloudStack",
+		locations: "US"
+	}
+};
